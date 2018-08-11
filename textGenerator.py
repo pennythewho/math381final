50fe212b4f189a3ad53204767206922709dd3947
pennythewho <pennespinoza+git@gmail.com>
2018-08-10 22:04:38 -0700

    allow forcing a starting phrase when generating text

--- textGenerator.py
+++ textGenerator.py
@@ -3,10 +3,21 @@ from itertools import accumulate
 
 from trainer import sentenceEndingPunc
 
-def generateText(srcGraph, targetLen=25):
+def generateText(srcGraph, targetLen=25, startPhrase=None):
+    """
+
+    :param srcGraph:
+    :param targetLen:
+    :param startPhrase:     allows user choosing the starting place for a graph
+                            if None or a phrase that doesn't exist in srcGraph, one will be chosen at random
+    :return:
+    """
     out = []
     random.seed(time.time())
-    out.append(list(srcGraph.keys())[random.randrange(len(srcGraph))]) # TODO: capitalize this later (not now, b/c then you might not find it in the srcGraph)
+    if not startPhrase or startPhrase not in srcGraph:
+        startPhrase = list(srcGraph.keys())[random.randrange(len(srcGraph))]
+    # TODO: capitalize this later (not now, b/c then you might not find it in the srcGraph)
+    out.append(startPhrase)
     # keep generating until the length exceeds the targetLen but also require finishing a sentence
     while len(out) < targetLen or out[-1][-1] not in sentenceEndingPunc:
         # get next phrases - needs to be sorted to match probabilities properly
