396add0f5f03caf5e0c8156bfb74078bfe3264ee
pennythewho <pennespinoza+git@gmail.com>
2018-08-12 16:43:42 -0700

    initial version of returning complete sentences only

--- textGenerator.py
+++ textGenerator.py
@@ -3,7 +3,7 @@ from itertools import accumulate
 
 from trainer import sentenceEndingPunc
 
-def generateText(srcGraph, targetLen=50, firstNgram=None, forceCap=False):
+def generateText(srcGraph, targetLen=50, firstNgram=None, forceCap=False, includeIncompleteSentences=True):
     """
     :param targetLen:   the target number of words (NOT the target number of n-grams) in the generated text
                         this is just a target - we try to end the output near the end of a sentence so the output
@@ -11,6 +11,8 @@ def generateText(srcGraph, targetLen=50, firstNgram=None, forceCap=False):
     :param firstNgram:  allows user choosing the starting n-gram
                         if None or an n-gram that doesn't exist in srcGraph, one will be chosen at random
     :param forceCap:    if True, any word following a sentenceEndingPunc will be capitalized
+    :param includeIncompleteSentences:  if False, will strip words preceding the first space after the
+                        first sentence ending punctuation, and any words after the last sentence-ending punctuation
     :return:
     """
     out = []
@@ -29,7 +31,8 @@ def generateText(srcGraph, targetLen=50, firstNgram=None, forceCap=False):
         i = 0 if len(innerGraph) == 1 else next(x[0] for x in enumerate(npp) if x[1] >= random.random())
         out.append(nextPhrases[i] if not forceCap else _cappedNgram(nextPhrases[i], lastNgram))
         lastNgram = nextPhrases[i]
-    return ' '.join(out)
+        out = out[_getFirstSentenceIdx(out):]
+    return stripIncompleteSentences(' '.join(out)) if includeIncompleteSentences else ' '.join(out)
 
 def _cappedNgram(ngram, lastngram):
     # capitalize the first word if the lastngram exists and ended a sentence
@@ -43,7 +46,35 @@ def _cappedNgram(ngram, lastngram):
 def _isLongEnough(out, targetLen):
     # want it to be the target length but also have the last n-gram contain the end of a sentence
     n = len(out[-1].split())    # length of n-grams
-    return len(out) >= targetLen/n and any((c in sentenceEndingPunc) for c in out[-1])
+    begin = _getFirstSentenceIdx(out)
+    return len(out[begin:]) >= targetLen/n and any((c in sentenceEndingPunc) for c in out[-1])
+
+def _getFirstSentenceIdx(out):
+    lo = len(out)
+    begin = _findFirstInstance(out, sentenceEndingPunc, endIdx=lo)
+    # if the sentence-ending punctuation is at the end of the n-gram, return the index of the next n-gram
+    # but prevent stripping away the entire output so far
+    return begin + 1 if begin < lo - 1 and out[begin][-1] in sentenceEndingPunc else begin
+
+def _findFirstInstance(text, searchChars, beginIdx=0, endIdx=None, searchBackwards=False):
+    if endIdx == None:
+        endIdx = len(text)
+    inc = 1 if not searchBackwards else -1
+    try:
+        return next(i for i in range(beginIdx, endIdx, inc) for c in text[i] if c in searchChars)
+    except StopIteration:
+        return beginIdx
+
+
+def stripIncompleteSentences(text):
+    lt = len(text)
+    first = _findFirstInstance(text, sentenceEndingPunc, endIdx=lt)
+    first = _findFirstInstance(text, ' ', beginIdx=first, endIdx=lt) + 1
+    last = _findFirstInstance(text, sentenceEndingPunc, beginIdx=-1, endIdx=(-1 * lt + first - 1), searchBackwards=True) + 1
+    return text[first:None if last == 0 else last]
+
+
+
 
 
 
