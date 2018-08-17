af0607429458a265082f5d54b32bb8574b7ff0c4
Nick Cutler <nickcutler@outlook.com>
2018-08-16 21:26:52 -0700

    images for poster

--- nick_testing.py
+++ nick_testing.py
@@ -1,5 +1,6 @@
 import trainer, textGenerator, numpy
-graph = trainer.getGraph(trainer.parseFile('input/fellowship of the ring summary.txt'),1)
+words = trainer.parseFile('input/1liners.txt');
+graph = trainer.getGraph(words,2)
 
 #%%
 #helper1 = list(graph.keys());
@@ -8,11 +9,29 @@ graph = trainer.getGraph(trainer.parseFile('input/fellowship of the ring summary
 #for i in range(len(helper2)):
 #    helper3.append(list(helper2[i].keys())[0])
 #%%
-#import transitionMatrix
-#t_mat = transitionMatrix.transitionMatrix(graph)
+import transitionMatrix
+t_mat = transitionMatrix.transitionMatrix(graph)
+#%%
+import matplotlib
+from matplotlib.pyplot import *
+#%%
+matplotlib.pyplot.imsave('1liners_2.png',t_mat,cmap=cm.get_cmap('afmhot'))
+
 #%%
 #import matplotlib.pyplot as plt
 #plt.imshow(t_mat)
 #%%
 # generate some text
-textGenerator.generateText(graph,43,includeIncompleteSentences=False)
+textGenerator.generateText(graph,45,includeIncompleteSentences=True, firstNgram='Master')
+
+#%%
+from random import shuffle
+words2 = (list(words))
+#%%
+shuffle(words2)
+#%%
+graph2 = trainer.getGraph(words2,2)
+#%%
+t_mat2 = transitionMatrix.transitionMatrix(graph2)
+#%%
+matplotlib.pyplot.imsave('1liners_2_shuff.png',t_mat2,cmap=cm.get_cmap('afmhot'))
